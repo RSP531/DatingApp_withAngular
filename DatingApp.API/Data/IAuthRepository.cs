7c358c204b2a969814811771a2318f4390c9aace
Rob Peschke <rspesc31@gmail.com>
2020-05-13 13:26:54 -0400

    added the three methods to create a user, login, and verify if user exists

---
+++ DatingApp.API/Data/IAuthRepository.cs
@@ -0,0 +1,15 @@
+using System.Threading.Tasks;
+using DatingApp.API.Models;
+
+namespace DatingApp.API.Data
+{
+    public interface IAuthRepository
+    {
+        //register user
+        Task<User> Register(User user, string password);
+        //login to api
+        Task<User> Login(User username, string password);
+        //check if user exists
+        Task<bool> UserExists(string username);
+    }
+}
