736f6a91b7519d3ccd193bf445674fcae92e1e4b
Rob Peschke <rspesc31@gmail.com>
2020-05-14 16:07:09 -0400

    retrieving proper data from the database

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
@@ -11,13 +10,7 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ValueComponent,
-    NavComponent,
-    HomeComponent,
-    RegisterComponent
-  ],
+  declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [AuthService],
   bootstrap: [AppComponent]
