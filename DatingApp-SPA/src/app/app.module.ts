deb34a2c41337312aefb5065158b2c1f1275ce92
Rob Peschke <rspesc31@gmail.com>
2020-05-14 15:04:02 -0400

    adding sign-up page:

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -7,9 +7,17 @@ import { HttpClientModule } from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AppComponent, ValueComponent, NavComponent],
+  declarations: [
+    AppComponent,
+    ValueComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
+  ],
   imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [AuthService],
   bootstrap: [AppComponent]
