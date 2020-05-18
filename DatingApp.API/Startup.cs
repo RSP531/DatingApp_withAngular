061ded7511e74f97a14135b4fcb43212222d064e
Rob Peschke <rspesc31@gmail.com>
2020-05-18 10:55:51 -0400

    finished and working properly

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -73,8 +73,10 @@ namespace DatingApp.API
             }
             else 
             {
-              app.UseExceptionHandler(builder => {
-                builder.Run(async context => {
+              app.UseExceptionHandler(builder => 
+              {
+                builder.Run(async context => 
+                {
                   context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
 
                   var error = context.Features.Get<IExceptionHandlerFeature>();
