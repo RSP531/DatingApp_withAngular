04deab3fdd597f935749b1d940040ef2791ca42b
Rob Peschke <rspesc31@gmail.com>
2020-05-19 22:35:07 -0400

    gosh I am getting tired. I missed a $ in my users controller, small bug, but big problems

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -23,7 +23,7 @@ namespace DatingApp.API.Controllers
       
       return Ok(users);
     }
-    [HttpGet("${id}")]
+    [HttpGet("{id}")]
     public async Task<IActionResult> GetUser(int id)
     {
       var user = await _repo.GetUser(id);
